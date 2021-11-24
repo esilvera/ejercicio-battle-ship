b61759924172fd9019d95d7b4bbb0ab4f086c956
Edwards Silvera <edwards.silvera@gmail.com>
2021-11-24 19:08:22 +0000

    Inicio Proyecto

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@popperjs/core';
+import 'bootstrap/dist/js/bootstrap.bundle';
+
+import "./styles/styles.css"
+import Home from './Home';
+
+
+ReactDOM.render(<Home />, document.querySelector("#app"));

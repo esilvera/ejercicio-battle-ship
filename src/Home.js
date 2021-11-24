0d97456c2dbe8366f232c6b71cd029388d9d7e55
Edwards Silvera <edwards.silvera@gmail.com>
2021-11-24 21:36:10 +0000

    validado y guardado de naves tablero 1

--- src/Home.js
+++ src/Home.js
@@ -3,7 +3,7 @@ import TablePlayer1 from "./views/TablePlayer1";
 
 const Home = () => {
     const [board1, setBoard1] = useState([
-        [1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -28,40 +28,54 @@ const Home = () => {
     ]);
 
     let position = 0
+    let table = ""
 
-    const [shipPosition, setShipPosition] = useState("");
-    const [shipSize, setShipSize] = useState("");
-    
-    const positionShip = (index, sIndex) => {
-        console.log(index)
-        console.log(sIndex)
-        console.log(shipSize)
-        if (shipPosition !== "") {
-            for (var i = 0; i < shipSize; i++){
-                if (shipPosition === "horizontal"){
-                    let newArray = [...board1];
-                    newArray[index][sIndex+i] = 2
-                    setBoard1(newArray)
-                    console.log(board1[index][sIndex])
-                }else {
-                    let newArray = [...board1];
-                    newArray[index+i][sIndex] = 2
-                    setBoard1(newArray)
-                    console.log(board1[index][sIndex])
+    const [shipSize1, setShipSize1] = useState(["", "", "", ""]);
+    const [shipPosition1, setShipPosition1] = useState("");
+    const [chosenSize1, setChosenSize1] = useState("");
+    const [shipSize2, setShipSize2] = useState(["", "", "", ""]);
+    const [shipPosition2, setShipPosition2] = useState("");
+    const [chosenSize2, setChosenSize2] = useState("");
+
+
+    const positionShip1 = (index, sIndex) => {
+        if (shipPosition1 !== "") {
+            console.log("zona tiene: ", board1[index][sIndex])
+            if (board1[index][sIndex] === 0) {
+                for (var i = 0; i < chosenSize1; i++) {
+                    if (shipPosition1 === "horizontal") {
+                        let newArray = [...board1];
+                        newArray[index][sIndex + i] = 1
+                        setBoard1(newArray)
+                    } else {
+                        let newArray = [...board1];
+                        newArray[index + i][sIndex] = 1
+                        setBoard1(newArray)
+                    }
                 }
+                console.log(board1)
+                setShipPosition1("");
+                setChosenSize1("");
+            } else {
+                alert("Por favor elija otra zona")
             }
-            console.log(board1)
-            setShipPosition("");
-            setShipSize("");            
         } else {
-            alert("por favor escoja una nave")
+            alert("Por favor escoja una nave")
         }
-
     }
 
     const selectShip = (position, size) => {
-        setShipPosition(position);
-        setShipSize(size);
+        let newArray = [...shipSize1];
+
+        if (newArray[size] === "" || newArray[size] !== size) {
+            newArray[size] = size;
+            setShipSize1(newArray);
+            console.log("shipSize tiene: ", shipSize1)
+            setShipPosition1(position);
+            setChosenSize1(size);
+        } else {
+            alert("Nave ya asignada por favor elija otra")
+        }
     }
 
 
@@ -72,6 +86,7 @@ const Home = () => {
             <div className="container">
                 <div className="row">
                     <div className="col-md-5">
+                        <h3>Jugador 1</h3>
                         <div className="board">
                             {board1.length > 0 &&
                                 board1.map((newGrid, index) => {
@@ -85,7 +100,8 @@ const Home = () => {
                                                         className="grid"
                                                         id={position}
                                                         onClick={() => {
-                                                            positionShip(index, sIndex);
+                                                            positionShip1(index, sIndex);
+                                                            table = "board1"
                                                         }}>
                                                     </div>
                                                 )
@@ -97,6 +113,7 @@ const Home = () => {
                         </div>
                     </div>
                     <div className="col-md-5">
+                        <h3>Jugador 2</h3>
                         <div className="board">
                             {board2.length > 0 &&
                                 board2.map((newGrid, index) => {
@@ -111,6 +128,7 @@ const Home = () => {
                                                         id={position}
                                                         onClick={() => {
                                                             alert("Dio Click en el Dos")
+                                                            table = "board2"
                                                         }}>{ }
                                                     </div>
                                                 )
@@ -122,7 +140,7 @@ const Home = () => {
                         </div>
                     </div>
                 </div>
-                <h4>Naves:</h4>
+                <br></br>
                 <div id="ships" className="row">
                     <div className="col">
                         <div>
@@ -134,22 +152,22 @@ const Home = () => {
                     <div className="col">
                         <div>
                             <span>Acorazado (Tamaño: 4)</span>
-                            <div className="horizontal" onClick={() => { alert("Dio Click en Horizontal") }}>Elija: Horizontal</div>
-                            <div className="vertical" onClick={() => { alert("Dio Click en Vertical") }}>Elija: Vertical</div>
+                            <div className="horizontal" onClick={(evento) => { selectShip(evento.target.className, 4) }}>Elija: Horizontal</div>
+                            <div className="vertical" onClick={(evento) => { selectShip(evento.target.className, 4) }}>Elija: Vertical</div>
                         </div>
                     </div>
                     <div className="col">
                         <div>
                             <span>Submarino (Tamaño: 3)</span>
-                            <div className="horizontal" onClick={() => { alert("Dio Click en Horizontal") }}>Elija: Horizontal</div>
-                            <div className="vertical" onClick={() => { alert("Dio Click en Vertical") }}>Elija: Vertical</div>
+                            <div className="horizontal" onClick={(evento) => { selectShip(evento.target.className, 3) }}>Elija: Horizontal</div>
+                            <div className="vertical" onClick={(evento) => { selectShip(evento.target.className, 3) }}>Elija: Vertical</div>
                         </div>
                     </div>
                     <div className="col">
                         <div>
                             <span>Destructor (Tamaño: 2)</span>
-                            <div className="horizontal" onClick={() => { alert("Dio Click en Horizontal") }}>Elija: Horizontal</div>
-                            <div className="vertical" onClick={() => { alert("Dio Click en Vertical") }}>Elija: Vertical</div>
+                            <div className="horizontal" onClick={(evento) => { selectShip(evento.target.className, 2) }}>Elija: Horizontal</div>
+                            <div className="vertical" onClick={(evento) => { selectShip(evento.target.className, 2) }}>Elija: Vertical</div>
                         </div>
                     </div>
                 </div>
